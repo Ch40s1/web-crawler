ed47f30d547144f0fb705cbc6751ea2e139ece27
Ch40s1 <114247747+Ch40s1@users.noreply.github.com>
2024-06-15 11:43:37 -0500

    url gets broken down into properties and wrote test for it

--- crawl.js
+++ crawl.js
@@ -1,6 +1,9 @@
-function normalizeURL(url) {
-	const urlObj = new URL(url)
-	return fullPath
+export function normalizeURL(url) {
+  const urlObj = new URL(url);
+  let fullPath = `${urlObj.host}${urlObj.pathname}`;
+  // checks if last character is a /
+  if (fullPath.slice(-1) === "/") {
+    fullPath = fullPath.slice(0, -1);
+  }
+  return fullPath;
 }
-
-export { normalizeURL }
--- crawl.test.js
+++ crawl.test.js
@@ -1,6 +1,30 @@
+import { normalizeURL } from "./crawl.js";
+import { test, expect } from "@jest/globals";
 
-import { normalizeURL } from './crawl.js'
-import { test, expect } from '@jest/globals'
+test("normalizeUrl no safe protocal", () => {
+  const input = "https://boot.dev";
+  const actual = normalizeURL(input);
+  const expected = "boot.dev";
+  expect(actual).toEqual(expected);
+});
 
+test("normalizeUrl no protocal", () => {
+  const input = "http://boot.dev";
+  const actual = normalizeURL(input);
+  const expected = "boot.dev";
+  expect(actual).toEqual(expected);
+});
 
+test("normalizeUrl no safe protocal", () => {
+  const input = "https://boot.dev";
+  const actual = normalizeURL(input);
+  const expected = "boot.dev";
+  expect(actual).toEqual(expected);
+});
 
+test("normalizeUrl with path", () => {
+  const input = "https://boot.dev/path";
+  const actual = normalizeURL(input);
+  const expected = "boot.dev/path";
+  expect(actual).toEqual(expected);
+});
