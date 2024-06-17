4dfbde3a42e928521c5bc605805da7e3a034c142
Ch40s1 <114247747+Ch40s1@users.noreply.github.com>
2024-06-17 17:11:59 -0500

    added crawl function, main checks cli arguments

--- crawl.js
+++ crawl.js
@@ -1,3 +1,5 @@
+import { JSDOM } from 'jsdom'
+
 export function normalizeURL(url) {
   const urlObj = new URL(url);
   let fullPath = `${urlObj.host}${urlObj.pathname}`;
@@ -8,9 +10,60 @@ export function normalizeURL(url) {
   return fullPath;
 }
 
-export function getURLsFromHTML(htmlString, baseURL) {
-  // takes string of html, return list of all the tags in the html
-  // if the href is a path and not another url then add to base url then add to list
-  // if the url is another url that is not the base url then add to a list
-  pass;
+export function getURLsFromHTML(html, baseURL) {
+  const urls = []
+  const dom = new JSDOM(html)
+  const anchors = dom.window.document.querySelectorAll('a')
+
+  for (const anchor of anchors) {
+    if (anchor.hasAttribute('href')) {
+      let href = anchor.getAttribute('href')
+
+      try {
+        // convert any relative URLs to absolute URLs
+        href = new URL(href, baseURL).href
+        urls.push(href)
+      } catch (err) {
+        console.log(`${err.message}: ${href}`)
+      }
+    }
+  }
+
+  return urls
+}
+
+export async function crawlPage(currentURL) {
+  // fetch and parse the html of the currentURL
+  console.log(`crawling ${currentURL}`)
+
+  let res
+  try {
+    res = await fetch(currentURL)
+  } catch (err) {
+    throw new Error(`Got Network error: ${err.message}`)
+  }
+
+  if (res.status > 399) {
+    console.log(`Got HTTP error: ${res.status} ${res.statusText}`)
+    return
+  }
+
+  const contentType = res.headers.get('content-type')
+  if (!contentType || !contentType.includes('text/html')) {
+    console.log(`Got non-HTML response: ${contentType}`)
+    return
+  }
+
+  console.log(await res.text())
+
+  /* Make sure the currentURL is on the same domain as the baseURL. If it's not, just return the current pages. We don't want to crawl the entire internet, just the domain in question.
+  Get a normalized version of the currentURL.
+  If the pages object already has an entry for the normalized version of the current URL, just increment the count and return the current pages.
+  Otherwise, add an entry to the pages object for the normalized version of the current URL, and set the count to 1.
+  If we've gotten here, break out the logic for fetching the current URL and parsing the HTML into its own function. This will keep the crawl functionality readable and manageable. Call the new function inside crawlPage.
+  Assuming all went well with the fetch request in the new function, get all the URLs from the response body HTML.
+  Recursively crawl each URL you found on the page and update the pages to keep an aggregate count.
+  Finally, return the updated pages object.
+  */
+
 }
